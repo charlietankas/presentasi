c315f44f305dab7f600bc4a7553eb575a5a5d465
charlietankas <charlietankas98@gmail.com>
2019-12-06 20:58:06 +0700

    Menambah dropdown menu dan background slide

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,3 +18,12 @@ bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   multimedia(),
   extern(bespoke)
 ]);
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
